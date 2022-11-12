34d76331f578fda9ad09a03c6d1faf7ea9d35af3
LukeNK <lukenguyenkhanh@gmail.com>
2022-11-12 12:05:48 -0800

    Added navbar

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
+let fs = require('fs');
 
 const dict = require('../public/dict.json')
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Legal dictionary', dict: dict });
+  res.render('index', { title: 'Legal dictionary', dict: JSON.parse(fs.readFileSync('./public/dict.json', 'utf-8')) });
 });
 
 module.exports = router;
