6204c8439f4dff5b6795e7c6f7e9c92ad6e46480
LukeNK <lukenguyenkhanh@gmail.com>
2022-11-12 10:43:57 -0800

    Mostly finished front end

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
 
+const dict = require('../public/dict.json')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Legal dictionary' });
+  res.render('index', { title: 'Legal dictionary', dict: dict });
 });
 
 module.exports = router;
